7b38fd88f3d10d2006880e98c73bf0f8ac5c432a
Fangzsx <jygrzn@gmail.com>
2023-03-11 15:44:48 +0800

    version 4

--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -7,7 +7,7 @@ import { useNavigate } from 'react-router-dom';
 import SignIn from './signin.jsx';
 
 export default function Home(){
-    //test update feature1 version 3
+    //test update feature1 version 4 from feature branch
     const user = useSelector(state => state.user.value);
     const navigate = useNavigate();
     const [ showModal, setShowModal ] = useState(false);
