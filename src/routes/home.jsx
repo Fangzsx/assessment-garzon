f7f7adbb4f578bda9deaf0927cc5b65f20d842db
Fangzsx <jygrzn@gmail.com>
2023-03-11 15:24:02 +0800

    some updates

--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -7,6 +7,7 @@ import { useNavigate } from 'react-router-dom';
 import SignIn from './signin.jsx';
 
 export default function Home(){
+    //test update feature1
     const user = useSelector(state => state.user.value);
     const navigate = useNavigate();
     const [ showModal, setShowModal ] = useState(false);
---
+++ src/test.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+
+export default function test() {
+    useEffect(() => {
+      
+    
+      return () => {
+        second
+      }
+    }, [third])
+    
+
+  return (
+    <div>
+        
+    </div>
+  )
+}
